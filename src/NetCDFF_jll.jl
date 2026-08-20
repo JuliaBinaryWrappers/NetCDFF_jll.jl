24dbeb4d7f805dee6cf98be5674c6309a6c2e2ac
jlbuild <juliabuildbot@gmail.com>
2026-08-20 14:34:51 +0000

    NetCDFF_jll build 4.6.4+0

--- src/NetCDFF_jll.jl
+++ src/NetCDFF_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("NetCDFF")
-JLLWrappers.@generate_main_file("NetCDFF", UUID("78e728a9-57fe-5d11-897c-5014b89e5f84"))
+JLLWrappers.@generate_main_file("NetCDFF", Base.UUID("78e728a9-57fe-5d11-897c-5014b89e5f84"))
 end  # module NetCDFF_jll
